972fe164bccca15e765af20665d2339a76ae77ba
Ivan Kuchin <ivan@workisfun.ru>
2011-01-27 15:37:00 +0300

    patched icon

--- GitXPatch.m
+++ GitXPatch.m
@@ -11,6 +11,11 @@
 @implementation GitXPatch
 
 + (void) load {
+	NSString *gitXPatchIconPath = [[NSBundle bundleForClass:[self class]] pathForImageResource:@"GitXPatch"];
+	NSImage *gitXPatchIcon = [[NSImage alloc] initByReferencingFile:gitXPatchIconPath];
+	[NSApp setApplicationIconImage:gitXPatchIcon];
+	[gitXPatchIcon release];
+
 	NSMenuItem *closeItem = [[[[NSApp mainMenu] itemWithTitle:@"File"] submenu] itemWithTitle:@"Close"];
 	[closeItem setTarget:self];
 
